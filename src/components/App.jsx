fee3eef9a44e93c06e3e046395c6ceab3b13edc0
Beni-Alexandre <honfogabeni@gmail.com>
2025-02-05 12:08:14 +0000

    AOS added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import Header from "./header";
 import Courses from "./Courses";
 import Campus from "./campus";
@@ -6,22 +6,37 @@ import Facility from "./facilities";
 import Testimonials from "./testimonials";
 import Cta from "./Cta";
 import Footer from "./footer";
-
-
+import "animate.css/animate.compat.css"
+import ScrollAnimation from 'react-animate-on-scroll'
+import FadeIn from 'react-fade-in';
 
 
 
 function App() {
   return (
     <div >
+      <FadeIn>
       <Header />
+      </FadeIn>
+      
+      <ScrollAnimation animateIn="fadeIn">
       <Courses />
+      </ScrollAnimation>
+      <ScrollAnimation animateIn="fadeIn">
       <Campus />
+      </ScrollAnimation>
+      <ScrollAnimation animateIn="fadeIn">
       <Facility />
+      </ScrollAnimation>
+      <ScrollAnimation animateIn="fadeIn">
       <Testimonials />
+      </ScrollAnimation>
+      <ScrollAnimation animateIn="fadeIn">
       <Cta />
+      </ScrollAnimation>
+      <ScrollAnimation animateIn="fadeIn">
       <Footer />
-
+      </ScrollAnimation>
     </div>
   )
 }
